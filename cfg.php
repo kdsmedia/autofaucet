c1efdeed1246a74df10d3c7a5fea8fce09eb9197
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-04-28 08:19:58 +0700

    cfg.php

--- cfg.php
+++ cfg.php
@@ -6,8 +6,8 @@ Fill in your cookie and user-agent
 by capturing web autofaucet.org
 */
 
-$user_agent = "xxxxxxxxxx";
-$cookie = "xxxxxxxxxx";
+$user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
+$cookie = "_ga=GA1.1.240759546.1713760515; _ga_STC3CT2301=GS1.1.1714266497.2.1.1714267155.0.0.0; PHPSESSID=6f02cf97ec6596d8abe2f8fcc5ba5591; bitmedia_fid=eyJmaWQiOiJjZWU3M2RmNjg3NGVkOGNlMTMwNjEwN2FiN2VkMjA1MCIsImZpZG5vdWEiOiJlNTQ0ZGMyNTkyMGYzMjYyMjNiNzYwYmRjMDgwODFlOSJ9; uhash=003a89d1695da5a911d510d5e9c237a4; uhash=003a89d1695da5a911d510d5e9c237a4";
 
 
 ?>
