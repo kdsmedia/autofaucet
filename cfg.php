fcacd8cd0a7e734ebeb3fbcef7a11313d2305e2b
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-10 13:22:06 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -7,7 +7,7 @@ by capturing web autofaucet.org
 */
 
 $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
-$cookie = "_ga=GA1.1.240759546.1713760515; _ga_STC3CT2301=GS1.1.1714266497.2.1.1714267155.0.0.0; PHPSESSID=6f02cf97ec6596d8abe2f8fcc5ba5591; bitmedia_fid=eyJmaWQiOiJjZWU3M2RmNjg3NGVkOGNlMTMwNjEwN2FiN2VkMjA1MCIsImZpZG5vdWEiOiJlNTQ0ZGMyNTkyMGYzMjYyMjNiNzYwYmRjMDgwODFlOSJ9; uhash=003a89d1695da5a911d510d5e9c237a4; uhash=003a89d1695da5a911d510d5e9c237a4";
+$cookie = "bitmedia_fid=eyJmaWQiOiJmOTJmZTExYTU1Y2VlMzUwMWNhYjVkODc2MTcxMmU3OCIsImZpZG5vdWEiOiI1ZWMzMTgzY2M5NmIwODViOTUxODc1Y2NkODYzNjMzMCJ9; captcha=recaptchav2; captcha=recaptchav2; captcha=recaptchav2; captcha=recaptchav2; captcha=gpcaptcha; captcha=recaptchav2; captcha=recaptchav2; ci_session=jrb7vnl590uhhgu75gu0vg1tuv7krflm";
 
 
 ?>
